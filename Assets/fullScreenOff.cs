40175ea345d88fbd8ad63389c050bc70931f0dbb
Giuseppe Di Marzo <g.dimarzo@hotmail.it>
2018-09-10 22:14:21 +0200

    Primo commit

---
+++ Assets/fullScreenOff.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fullScreenOff : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.fullScreen = false;
+	}
+
+    // Update is called once per frame
+    uint exitCount = 0;
+    void Update () {
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //    Application.Quit();
+
+        //////
+        ///
+
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+        
+            exitCount++;
+            if (!IsInvoking("disableDoubleClick"))
+                Invoke("disableDoubleClick", 0.3f);
+        }
+
+
+        if (exitCount == 2)
+        {
+            CancelInvoke("disableDoubleClick");
+            Application.Quit();
+        }
+        
+
+
+        ////////
+    }
+
+    void disableDoubleClick()
+    {
+        exitCount = 0;
+    }
+
+}
